dcb512eef7c4b6b8856995acb78c5562730f4831
Piotr Tutak <p.tutak@outlook.com>
2019-04-07 11:31:51 +0200

    refactoring

--- test_shUnit.sh
+++ test_shUnit.sh
@@ -7,51 +7,50 @@ source "$SH_UNIT_DIR/test"
 
 
 test_run() {
-    local test_name=test_test_method
-    local test_method_was_run="${test_name}_WAS_RUN"
-    local test_method_set_up="${test_name}_WAS_SET_UP"
+    local test_name=test_method
+    local test_was_run="${test_name}_WAS_RUN"
     run "$test_name"
-    if ! "${!test_method_was_run}"; then
+    if ! "${!test_was_run}"; then
         return 1
     fi
     return 0
 }
 
 test_set_up_test() {
-    local test_name=test_test_method
-    local test_method_set_up="${test_name}_WAS_SET_UP"
+    local test_name=test_method
+    local test_set_up="${test_name}_WAS_SET_UP"
     set_up_test "$test_name"
-    if ! "${!test_method_set_up}"; then
+    if ! "${!test_set_up}"; then
         return 1
     fi
     return 0
 }
 
 test_was_run() {
-    local test_name=test_test_method
-    local test_method_was_run="${test_name}_WAS_RUN"
-    if "${!test_method_was_run}"; then
+    local test_name=test_method
+    local test_was_run="${test_name}_WAS_RUN"
+    if "${!test_was_run}"; then
         return 1
     fi
     was_run "$test_name"
-    if ! "${!test_method_was_run}"; then
+    if ! "${!test_was_run}"; then
         return 1
     fi
     return 0
 }
 
 test_tear_down_test() {
-    local test_name=test_test_method
-    local test_method_tear_down="${test_name}_TEAR_DOWN"
+    local test_name=test_method
+    local test_tear_down="${test_name}_TEAR_DOWN"
     tear_down_test "$test_name"
-    if ! "${!test_method_tear_down}"; then
+    if ! "${!test_tear_down}"; then
         return 1
     fi
     return 0
 }
 
-test_test_method() {
-    test_method
+test_method() {
+    method
     return 0
 }
 
@@ -71,6 +70,7 @@ test_log_status() {
 
 test_execute_suite() {
     local suite_name="$1"
+
 }
 
 run test_tear_down_test
@@ -78,5 +78,5 @@ run test_another_method
 run test_set_up_test
 run test_was_run
 run test_run
-run test_test_method
+run test_method
 run test_log_status
